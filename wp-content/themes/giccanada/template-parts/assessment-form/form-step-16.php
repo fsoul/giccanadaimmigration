4e9e41776e3ff018e008724ef8d6edd388b327f2
Aleksandr Rogovoy <rogovoyalexandr94@gmail.com>
2017-11-09 16:57:12 +0200

    step 16

--- wp-content/themes/giccanada/template-parts/assessment-form/form-step-16.php
+++ wp-content/themes/giccanada/template-parts/assessment-form/form-step-16.php
@@ -5,46 +5,48 @@
             <label>Есть ли у вас дети?</label>
             <section>
                 <input name="child-relation" class="ass-radio" value="y" checked type="radio"
-                       id="child-relation-y">
+                       id="child-relation-y" data-role="radio">
                 <label for="child-relation-y" >Да</label>
             </section>
             <section>
-                <input name="child-relation" class="ass-radio" value="n" type="radio" id="child-relation-n">
+                <input name="child-relation" class="ass-radio" value="n" type="radio" id="child-relation-n" data-role="radio">
                 <label for="child-relation-n" >Нет</label>
             </section>
         </section>
         <section>
             <label for="child-surname-0">Фамилия</label>
-            <input type="text" name="child-surname[]" id="child-surname-0" class="to-change-id">
+            <input type="text" name="child-surname[]" id="child-surname-0" class="to-change-id" data-role="text">
             <span class="error-text to-change-id" id="error-child-surname-0"></span>
         </section>
         <section>
             <label for="child-name-0" class="to-change-id">Имя</label>
-            <input type="text" name="child-name[]" id="child-name-0" class="to-change-id">
+            <input type="text" name="child-name[]" id="child-name-0" class="to-change-id" data-role="text">
             <span class="error-text to-change-id" id="error-child-name-0"></span>
         </section>
         <section class="combine-date birth-date">
-            <label>Дата рождения</label>
-            <div>
-                <select title="" name="child-birth-day[]" class="day to-change-id" id="child-birth-day-0" required
-                        data-class="child-birth-date-0">
-                    <option value="" disabled selected>Day</option>
-				    <?= getDayOptions(); ?>
-                </select>
+            <div class="to-change-id" data-role="combine-date" id="child-birth-date-0" data-msg="error-child-birth-date-0">
+                <label>Дата рождения</label>
+                <div>
+                    <select title="to-change-id" name="child-birth-day[]" class="day to-change-id" id="child-birth-day-0" required
+                            data-class="child-birth-date-0" data-role="select">
+                        <option value="" disabled selected>Day</option>
+			            <?= getDayOptions(); ?>
+                    </select>
 
-                <select title="" name="child-birth-month[]" class="month to-change-id" id="child-birth-month-0" required
-                        data-class="child-birth-date-0">
-                    <option value="" disabled selected>Month</option>
-				    <?= getMonthOptions(); ?>
-                </select>
+                    <select title="" name="child-birth-month[]" class="month to-change-id" id="child-birth-month-0" required
+                            data-class="child-birth-date-0" data-role="select">
+                        <option value="" disabled selected>Month</option>
+			            <?= getMonthOptions(); ?>
+                    </select>
 
-                <select title="" name="child-birth-year[]" class="year to-change-id" id="child-birth-year-0" required
-                        data-class="child-birth-date-0" data-role="combine-date">
-                    <option value="" disabled selected>Year</option>
-				    <?= getYearOptions(); ?>
-                </select>
+                    <select title="" name="child-birth-year[]" class="year to-change-id" id="child-birth-year-0" required
+                            data-class="child-birth-date-0" data-role="select">
+                        <option value="" disabled selected>Year</option>
+			            <?= getYearOptions(); ?>
+                    </select>
+                </div>
+                <span class="error-text to-change-id" id="error-child-birth-date-0"></span>
             </div>
-            <span class="error-text" id="error-child-birth-date-0"></span>
         </section>
     </div>
     <section>
