d3db54beb79418ac453fa1abdd1bed4d82d1eef1
Aleksandr Rogovoy <rogovoyalexandr94@gmail.com>
2017-11-13 14:14:53 +0200

    fix css email 1

--- wp-content/themes/giccanada/template-parts/emails/mail-footer.php
+++ wp-content/themes/giccanada/template-parts/emails/mail-footer.php
@@ -1,42 +1,68 @@
 <?php ?>
 <table style="width:100%;border-spacing:0;border-collapse:collapse;">
-	<tbody>
-	<tr style="font-weight:bold;font-size:.85em;">
-		<td style="font-family:'Lato',sans-serif;padding-top:20px;">
-			<p style="margin:0;">Остались вопросы? Звоните!</p>
-			<p style="margin:0;padding-bottom:20px">
-				<a style="color:#fff;text-decoration:none"
-				   href="tel:+03-6738333">03-6738333</a>
-				<a style="color:#fff;text-decoration:none"
-				   href="tel:+054-3319797">054-3319797</a>
-				<a style="color:#fff;text-decoration:none" href="tel:+16475584910">+16475584910</a>
-			</p>
-
-			<p style="margin:0;">Отдел продаж:</p>
-			<a href="mailto:Info@giccanadaimmigration.com"
-			   style="margin:0;padding-bottom:20px;display:block;color:#fff;text-decoration:none">Info@giccanadaimmigration.com</a>
-
-			<p style="margin:0;">Отдел обслуживания:</p>
-			<a href="mailto:development.gic@gmail.com"
-			   style="margin:0;display:block;color:#fff;text-decoration:none">development.gic@gmail.com</a>
-			<a href="mailto:gicinfocenter2@gmail.com"
-			   style="margin:0;display:block;color:#fff;text-decoration:none">gicinfocenter2@gmail.com</a>
-		</td>
-		<td style="font-family:'Lato',sans-serif;padding-top:20px;vertical-align: top;">
-			<a style="margin:0;display:block;color:#fff;text-decoration:none"">714а, ул.Антоновича
-			172, Киев, Украина</a>
-			<p style="margin:0;padding-bottom:20px">Бизнес центр Палладиум, 7 этаж офис, 719а</p>
-
-			<p style="margin:0;">Тель-Авив, ул.Нирим 3, Правый Лифт, 3-ий этаж</p>
-			<p style="margin:0;padding-bottom:20px">(Здание Канадского Посольства)</p>
-
-			<p style="margin:0;">Aventura, FL 33180 APT 1410, USA</p>
-		</td>
-	</tr>
-	<tr style="font-weight:bold;font-size:.85em;line-height:16px">
-		<td colspan="2" style="font-family:'Lato',sans-serif;padding-top:20px;">
-			<p style="margin:0;">&copy; <?= date("Y");?> GIC Global Immigration Corp. Все права защищены</p>
-		</td>
-	</tr>
-	</tbody>
+    <tbody>
+    <tr style="font-weight:bold;">
+        <td style="color:#fff;font-family:'Lato',sans-serif;padding-top:20px;">
+            <table>
+                <tr>
+                    <td style="color:#fff;font-family:'Lato',sans-serif;font-size:.8rem;">
+                        <p style="margin:0;">Остались вопросы? Звоните!</p>
+                        <p style="margin:0;padding-bottom:20px">
+                            <a style="color:#fff;text-decoration:none;display:block;;font-size:.7rem;"
+                               href="tel:+03-6738333">03-6738333</a>
+                            <a style="color:#fff;text-decoration:none;display:block;;font-size:.7rem;"
+                               href="tel:+054-3319797">054-3319797</a>
+                            <a style="color:#fff;text-decoration:none;display:block;;font-size:.7rem;" href="tel:+16475584910">+16475584910</a>
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="color:#fff;font-family:'Lato',sans-serif;font-size:.8rem;">
+                        <p style="margin:0;">Отдел продаж:</p>
+                        <a href="mailto:Info@giccanadaimmigration.com"
+                           style="margin:0;padding-bottom:20px;display:block;color:#fff;font-size:.63rem;text-decoration:none">Info@giccanadaimmigration.com</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="color:#fff;font-family:'Lato',sans-serif;font-size:.8rem;">
+                        <p style="margin:0;">Отдел обслуживания:</p>
+                        <a href="mailto:development.gic@gmail.com"
+                           style="margin:0;display:block;color:#fff;text-decoration:none;font-size:.63rem;">development.gic@gmail.com</a>
+                        <a href="mailto:gicinfocenter2@gmail.com"
+                           style="margin:0;display:block;color:#fff;text-decoration:none;font-size:.63rem;">gicinfocenter2@gmail.com</a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+        <td style="color:#fff;font-family:'Lato',sans-serif;padding-top:20px;vertical-align:top;">
+            <table>
+                <tr>
+                    <td style="color:#fff;font-family:'Lato',sans-serif;">
+                        <p style="margin:0;display:block;color:#fff;text-decoration:none;font-size:.8rem;">714а, ул.Антоновича
+                        172, Киев, Украина</p>
+                        <p style="margin:0;padding-bottom:20px;font-size:.8rem;">Бизнес центр Палладиум, 7 этаж офис, 719а</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="color:#fff;font-family:'Lato',sans-serif;font-size:.8rem;">
+                        <p style="margin:0;font-size:.8rem;">Тель-Авив, ул.Нирим 3, Правый Лифт, 3-ий этаж</p>
+                        <p style="margin:0;padding-bottom:20px;font-size:.8rem;">(Здание Канадского Посольства)</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td style="color:#fff;font-family:'Lato',sans-serif;font-size:.8em;">
+                        <p style="margin:0;font-size:.8rem;">Aventura, FL 33180 APT 1410, USA</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr style="font-weight:bold;line-height:16px;">
+        <td colspan="2"
+            style="color:#fff;font-family:'Lato',sans-serif;padding-top:20px;padding-bottom:40px;font-size:.8rem;">
+            <p style="margin:0;font-size:.8rem;">&copy; <?= date( "Y" ); ?> GIC Global Immigration Corp. Все права
+                защищены</p>
+        </td>
+    </tr>
+    </tbody>
 </table>
