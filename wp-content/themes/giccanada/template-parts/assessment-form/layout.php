6e13923c21f521764f83d710fe8184208a79ed5a
Aleksandr Rogovoy <rogovoyalexandr94@gmail.com>
2017-11-15 18:00:06 +0200

    в мобильной версии кнопки не нажимаются

--- wp-content/themes/giccanada/template-parts/assessment-form/layout.php
+++ wp-content/themes/giccanada/template-parts/assessment-form/layout.php
@@ -1,6 +1,11 @@
 <div class="modal fade" id="assessment-modal">
     <div class="modal-dialog  modal-lg" role="document">
         <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
             <div class="modal-body">
                 <div id="ass-step-init">
                     <div class="container-fluid">
