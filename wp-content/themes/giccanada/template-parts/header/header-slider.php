63466f5f828dc5f953dbe4734782bbd2714a510e
Aleksandr Rogovoy <rogovoyalexandr94@gmail.com>
2017-11-15 16:51:57 +0200

    в мобильной версии кнопки не нажимаются

--- wp-content/themes/giccanada/template-parts/header/header-slider.php
+++ wp-content/themes/giccanada/template-parts/header/header-slider.php
@@ -8,7 +8,7 @@
         </span>
     </div>
     <div class="row no-gutters justify-content-center">
-        <a href="#" class="col hs-item-button orange-btn">Начать процесс</a>
+        <a href="javascript:void(0);" data-toggle="modal" data-target="#assessment-modal" class="col hs-item-button orange-btn">Начать процесс</a>
     </div>
 </div> <!--header-mobile-content-->
 <div class="container-fluid carousel-div"> <!--header slider-->
