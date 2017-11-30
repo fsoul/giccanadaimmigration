306b75171fef82bdabbaae0f6222d83de2210b88
Aleksandr Rogovoy <rogovoyalexandr94@gmail.com>
2017-11-30 15:12:29 +0200

    fix css footer mobile

--- wp-content/themes/giccanada/template-parts/footer/footer-bottom.php
+++ wp-content/themes/giccanada/template-parts/footer/footer-bottom.php
@@ -1,7 +1,7 @@
 <?php ?>
 <div class="container-fluid">
 	<div class="row align-items-center no-gutters footer-bottom">
-		<div class="col-2 col-sm-3 col-md-2" id="footer-socials">
+		<div class="col-6 col-sm-2 col-md-2" id="footer-socials">
 			<!--footer-socials-->
 			<div class="row flex-nowrap">
                 <a href="#" class="col-2"><i class="fa fa-vk tb-menu-logo"></i></a>
@@ -9,13 +9,13 @@
                 <a href="#" class="col-2"><i class="fa fa-instagram tb-menu-logo"></i></a>
 			</div>
 		</div>
-        <div class="col-1 col-sm-2 col-md-1" id="footer-map">
+        <div class="col-6 col-sm-2 col-md-1" id="footer-map">
             <a href="#" class="footer-site-map white-link-underline">Карта сайта</a>
         </div>
-		<div class="col-9 col-sm-7 col-md-6">
-            <div class="row flex-nowrap">
-                <a href="javascript:void(0);" class="col-6 white-link-underline terms-modal">Условия использования</a>
-                <a href="javascript:void(0);" class="col-6 white-link-underline agreement-modal">Пользовательское соглашение и политика конфиденциальности</a>
+		<div class="col-12 col-sm-8 col-md-6">
+            <div class="row">
+                <a href="javascript:void(0);" class="col-12 col-sm-6 white-link-underline terms-modal">Условия использования</a>
+                <a href="javascript:void(0);" class="col-12 col-sm-6 white-link-underline agreement-modal">Пользовательское соглашение и политика конфиденциальности</a>
             </div>
         </div>
 		<div class="col-sm-12 col-md-3">
